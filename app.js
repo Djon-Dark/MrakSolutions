c8d3a22a3bb434af9a59da850ff43843495ccee6
Djon-Dark <mrak.ivan@gmail.com>
2021-01-20 18:13:33 +0100

    Gallery modal done. scroll buttons added

--- app.js
+++ app.js
@@ -207,4 +207,28 @@ const hidden = (element)=>{
         setTimeout(()=>{element.classList.add('hide')},400)}
 }
 
-// GALLERY AUTO ADD
+// GALLERY
+const modal = document.querySelector('.modal');
+const previews = document.querySelectorAll('.gallery img');
+const original = document.querySelector('.full-image');
+const caption = document.querySelector('.caption');
+
+previews.forEach(preview => {
+    preview.addEventListener('click', ()=>{
+        modal.classList.add('open');
+        original.classList.add('open')
+        //dynamic change text and image
+        const originalSrc = preview.getAttribute('data-original');
+        original.src = `./imgs/gallery/${originalSrc}`;
+        const altText = preview.alt;
+        caption.textContent = altText;
+    })
+})
+
+modal.addEventListener('click',(e)=>{
+    if(e.target.classList.contains('modal')){
+        modal.classList.remove('open');
+        original.classList.remove('open');
+    }
+})
+
